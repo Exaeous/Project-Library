2265b7cc1dcefaef01347bf88bcb478b1f3c24fd
Sam <120003855+Exaeous@users.noreply.github.com>
2025-10-19 18:31:56 +0530

    Add the main logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary(title, author, pages, status) {
+  let newBook = new Book(title, author, pages, status);
+  return myLibrary.push(newBook);
+}
+
+addBookToLibrary("test", "test", "test", "test");
